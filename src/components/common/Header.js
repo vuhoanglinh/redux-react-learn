107e2bb273b9c833eb7bcc491791519f2950b9ec
Linh Vu <vuhoanglinh2002@gmail.com>
2016-12-09 11:50:44 +0700

    Create Preloader Component

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,14 +1,16 @@
 import React, { PropTypes } from 'react';
 import { Link, IndexLink } from 'react-router';
+import LoadingDots from './LoadingDots';
 
 const Header = () => {
   return (
     <nav>
       <IndexLink to="/" activeClassName="active">Home</IndexLink>
       {" | "}
-      <Link to="/about" activeClassName="active">About</Link>
-      {" | "}
       <Link to="/courses" activeClassName="active">Coures</Link>
+      {" | "}
+      <Link to="/about" activeClassName="active">About</Link>
+      <LoadingDots interval={100} dots={200}/>
     </nav>
   );
 };
