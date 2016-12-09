44390a813596ef0f36bc7208eb8c880550e13d2a
Linh Vu <vuhoanglinh2002@gmail.com>
2016-12-09 17:44:20 +0700

    Add custome course valid

--- src/components/course/ManageCoursePage.js
+++ src/components/course/ManageCoursePage.js
@@ -3,7 +3,8 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import * as courseActions from '../../actions/courseActions';
 import CourseForm from './CourseForm';
-import toasrt from 'toastr';
+import {authorsFormattedForDropdown} from '../../selectors/selectors';
+import toastr from 'toastr';
 
 class ManageCoursePage extends React.Component {
   constructor(props, context) {
@@ -34,20 +35,39 @@ class ManageCoursePage extends React.Component {
     return this.setState({course: course});
   }
 
+  courseFormIsValid() {
+    let formIsValid = true;
+    let errors = {};
+
+    if (this.state.course.title.length < 5) {
+      errors.title = 'Title must be at least 5 characters.';
+      formIsValid = false;
+    }
+
+    this.setState({errors: errors});
+    return formIsValid;
+  }
+
   saveCourse(event) {
     event.preventDefault();
+
+    if (!this.courseFormIsValid()) {
+      return;
+    }
+
     this.setState({saving: true});
+
     this.props.actions.saveCourse(this.state.course)
       .then(() => this.redirect())
       .catch(error => {
-        toasrt.error(error);
+        toastr.error(error);
         this.setState({saving: false});
       });
   }
 
   redirect() {
     this.setState({saving: false});
-    toasrt.success('Course saved');
+    toastr.success('Course saved');
     this.context.router.push('/courses');
   }
 
@@ -91,16 +111,9 @@ function mapStateToProps(state, ownProps) {
     course = getCourseById(state.courses, courseId);
   }
 
-  const authorsFormattedForDropdown = state.authors.map(author => {
-    return {
-      value: author.id,
-      text: author.firstName + ' ' + author.lastName
-    };
-  });
-
   return {
     course: course,
-    authors: authorsFormattedForDropdown
+    authors: authorsFormattedForDropdown(state.authors)
   };
 }
 
