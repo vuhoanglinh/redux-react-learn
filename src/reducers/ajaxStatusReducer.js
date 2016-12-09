1ee76f796be30b03b90bace43ee84ab565d9f95c
Linh Vu <vuhoanglinh2002@gmail.com>
2016-12-09 16:33:21 +0700

    config time delay and add error handling when call ajax error

--- src/reducers/ajaxStatusReducer.js
+++ src/reducers/ajaxStatusReducer.js
@@ -1,15 +1,16 @@
 import * as types from '../actions/actionTypes';
-import intialState from './initialState';
+import initialState from './initialState';
 
 function actionTypeEndsInSucess(type) {
   return type.substring(type.length - 8) == '_SUCCESS';
 }
 
-export default function ajaxStatusReducer(state = intialState.ajaxCallsInProgress, action) {
+export default function ajaxStatusReducer(state = initialState.ajaxCallsInProgress, action) {
   if(action.type == types.BEGIN_AJAX_CALL) {
     return state + 1;
   }
-  else if (actionTypeEndsInSucess(action.type)) {
+  else if (action.type == types.AJAX_CALL_ERROR ||
+    actionTypeEndsInSucess(action.type)) {
     return state - 1;
   }
 
